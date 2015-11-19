8e38210402e3b293cf55c5cbc6b13741da9e75af
madebyafox <amyraefox@gmail.com>
2015-11-18 20:25:27 -0800

    reorg code w/ js folders + save file on enter key

---
+++ js/helpers.js
@@ -0,0 +1,37 @@
+
+//----------------------
+///CALLED EVERYTIME A KEY IS PRESSED 
+//----------------------
+function doKeyDown(e) 
+{
+	switch(e.keyCode)
+	{
+		case 13:
+			console.log("ENTER KEY PRESSED");		
+			saveData(data);			
+			break;
+	}
+}
+
+
+
+//----------------------
+///CALLED ON ENTER KEY PRESS TO DOWLOAD CSV DATA 
+//----------------------
+function saveData(data)
+{
+	var csvContent = "data:text/csv;charset=utf-8,";
+	data.forEach(function(infoArray, index){
+
+	   dataString = infoArray.join(",");
+	   csvContent += index < data.length ? dataString+ "\n" : dataString;
+
+	});
+	var encodedUri = encodeURI(csvContent);
+	var link = document.createElement("a");
+	link.setAttribute("href", encodedUri);
+	link.setAttribute("download", "my_data.csv");
+
+	link.click(); // This will download the data file named "my_data.csv".
+
+}
